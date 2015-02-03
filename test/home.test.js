a06eec32e5a8d9759502450ad0da45f09e6ec3b9
piotrekbass <piotrek.koscielniak@gmail.com>
2015-02-03 22:00:32 +0100

    test clean up

--- test/home.test.js
+++ test/home.test.js
@@ -1,11 +1,22 @@
 var app = require('../app.js');
 var request = require('supertest').agent(app.listen());
 var co = require('co');
-var should = require('should');
+require('should');
 
+var testHelpers = require('./testHelpers');
 var db = require('./../lib/db');
 
 describe('The homepage', function () {
+
+    beforeEach(function (done) {
+        testHelpers.removeAllDocs();
+        done();
+    });
+
+    after(function () {
+        testHelpers.removeAllDocs();
+    });
+
     it('displays nicely without errors', function (done) {
         request
             .get('/')
