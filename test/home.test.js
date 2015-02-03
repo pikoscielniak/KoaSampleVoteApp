f37306a874aacfc9983c0a17e6ed7b4eadf46d38
piotrekbass <piotrek.koscielniak@gmail.com>
2015-02-03 21:41:24 +0100

    questions on home page

--- test/home.test.js
+++ test/home.test.js
@@ -1,5 +1,9 @@
 var app = require('../app.js');
 var request = require('supertest').agent(app.listen());
+var co = require('co');
+var should = require('should');
+
+var db = require('./../lib/db');
 
 describe('The homepage', function () {
     it('displays nicely without errors', function (done) {
@@ -8,5 +12,21 @@ describe('The homepage', function () {
             .expect(200)
             .expect('Content-Type', /html/)
             .end(done);
+    });
+
+    it("lists all the questions in the database", function (done) {
+        co(function * () {
+            yield db.questions.insert({title: 'Question Q1'});
+            yield db.questions.insert({title: 'Question Q2'});
+
+            request
+                .get("/")
+                .expect(200)
+                .expect(function (res) {
+                    res.text.should.containEql("Question Q1");
+                    res.text.should.containEql("Question Q2");
+                })
+                .end(done);
+        });
     });
 });
